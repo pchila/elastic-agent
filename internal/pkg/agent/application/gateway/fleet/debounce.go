42aae248551848f7fbfae3ca3d90bc3ae5ae06fd
Paolo Chila <paolo.chila@elastic.co>
2023-04-19 08:41:47 +0200

    Define new AccumulatorDebounce

--- internal/pkg/agent/application/gateway/fleet/debounce.go
+++ internal/pkg/agent/application/gateway/fleet/debounce.go
@@ -11,6 +11,8 @@ import (
 
 type debouncerFunc[T any] func(context.Context, <-chan T, time.Duration) <-chan T
 
+// type accumulatorDebouncerFunc[T any, R any] func(context.Context, <-chan T, time.Duration, int) <-chan R
+
 func Debounce[T any](ctx context.Context, in <-chan T, minDebounce time.Duration) <-chan T {
 
 	debounceTimer := time.NewTimer(minDebounce)
@@ -58,6 +60,85 @@ func debounceWithTimeSource[T any](ctx context.Context, in <-chan T, timeSrc <-c
 				minDebounceElapsed = true
 				if receivedNewValue {
 					outCh <- value
+					return
+				}
+			}
+		}
+	}()
+
+	return outCh
+}
+
+type TimestampedValue[T any] struct {
+	Time  time.Time
+	Value T
+}
+
+type Clock interface {
+	Now() time.Time
+}
+
+type stdlibClock struct{}
+
+func (stdC stdlibClock) Now() time.Time {
+	return time.Now()
+}
+
+func AccumulatorDebounce[T any, R []TimestampedValue[T]](ctx context.Context, in <-chan T, minDebounce time.Duration, maxItems int) <-chan R {
+
+	debounceTimer := time.NewTimer(minDebounce)
+	cancelDebounceTimer := func() {
+		if !debounceTimer.Stop() {
+			<-debounceTimer.C
+		}
+	}
+	return accumulatorDebounceWithTimeSources[T, R](ctx, in, debounceTimer.C, cancelDebounceTimer, new(stdlibClock), maxItems)
+}
+
+func accumulatorDebounceWithTimeSources[T any, R []TimestampedValue[T]](ctx context.Context, in <-chan T, timeSrc <-chan time.Time, stopTimeSrc func(), clock Clock, maxItems int) <-chan R {
+	outCh := make(chan R, 1)
+	go func() {
+		defer close(outCh)
+		var (
+			values             R
+			receivedNewValue   bool
+			minDebounceElapsed bool
+		)
+
+		for {
+			select {
+			case <-ctx.Done():
+				// TODO: Should we return value when context expires?
+				if receivedNewValue {
+					outCh <- values
+				}
+				stopTimeSrc()
+				return
+			case newValue, ok := <-in:
+				if !ok {
+					// input channel has closed, set it to nil  so we don't unblock here anymore
+					in = nil
+					continue
+				}
+				receivedNewValue = true
+				newTimestampedValue := TimestampedValue[T]{
+					Value: newValue,
+					Time:  clock.Now(),
+				}
+				if maxItems > 0 && len(values) == maxItems {
+					values = append(values[1:], newTimestampedValue)
+				} else {
+					values = append(values, newTimestampedValue)
+				}
+
+				if minDebounceElapsed {
+					outCh <- values
+					return
+				}
+			case <-timeSrc:
+				minDebounceElapsed = true
+				if receivedNewValue {
+					outCh <- values
 					return
 				}
 			}
--- internal/pkg/agent/application/gateway/fleet/debounce_test.go
+++ internal/pkg/agent/application/gateway/fleet/debounce_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDebouncer(t *testing.T) {
+func TestDebounce(t *testing.T) {
 
 	t.Run("Debounce cancels without value when context expires", func(t *testing.T) {
 
@@ -175,6 +175,245 @@ func TestDebouncer(t *testing.T) {
 		select {
 		case val, ok := <-outCh:
 			assert.Falsef(t, ok, "channel should be closed after the single value")
+			assert.Zero(t, val)
+		default:
+			t.Fatal("debouncer didn't close the output channel in time")
+		}
+
+	})
+}
+
+func TestAccumulatorDebounce(t *testing.T) {
+
+	t.Run("Debounce cancels without value when context expires", func(t *testing.T) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+		defer cancel()
+
+		// unused input channel
+		inCh := make(chan int)
+
+		debounceDuration := 1 * time.Minute
+		maxItems := 5
+		outCh := AccumulatorDebounce(ctx, inCh, debounceDuration, maxItems)
+
+		select {
+		case val, ok := <-outCh:
+			require.False(t, ok, "output channel didn't close as expected")
+			assert.Zero(t, val)
+		case <-time.After(100 * time.Millisecond):
+			t.Fatal("debouncer didn't cancel in time when cancelling context")
+		}
+
+	})
+	t.Run("Debounce cancels with value when context expires", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+		defer cancel()
+
+		// input channel used to send a single value
+		inCh := make(chan int)
+
+		values := []int{0, 1, 42, 66}
+		// send a values
+		go func() {
+			for _, v := range values {
+				inCh <- v
+			}
+
+		}()
+
+		debounceDuration := 1 * time.Minute
+
+		maxItems := 5
+		outCh := AccumulatorDebounce(ctx, inCh, debounceDuration, maxItems)
+
+		select {
+		case val, ok := <-outCh:
+			require.True(t, ok, "output channel closed before sending value")
+			actualValues := []int{}
+			for _, v := range val {
+				actualValues = append(actualValues, v.Value)
+			}
+			assert.Equal(t, values, actualValues)
+		case <-time.After(100 * time.Millisecond):
+			t.Fatal("debouncer didn't cancel in time when cancelling context")
+		}
+
+		select {
+		case val, ok := <-outCh:
+			require.False(t, ok, "output channel didn't close as expected")
+			assert.Zero(t, val)
+		default:
+			t.Fatal("debouncer didn't close the channel when cancelling context")
+		}
+
+	})
+
+	t.Run("Simple debounce after initial duration", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		// setup an input channel where we inject a series of values very quickly
+		inCh := make(chan int)
+
+		debounceDuration := 50 * time.Millisecond
+
+		values := []int{0, 1, 42, 66, 99, 100, 999}
+		// send all values
+		go func() {
+			for _, v := range values {
+				inCh <- v
+			}
+		}()
+
+		maxItems := 5
+		outCh := AccumulatorDebounce(ctx, inCh, debounceDuration, maxItems)
+
+		select {
+		case val, ok := <-outCh:
+			require.Truef(t, ok, "output channel closed unexpectedly")
+			actualValues := []int{}
+			for _, v := range val {
+				actualValues = append(actualValues, v.Value)
+			}
+			assert.Equal(t, values[len(values)-maxItems:], actualValues)
+		case <-ctx.Done():
+			t.Fatal("didn't receive a value before timeout")
+		}
+
+		select {
+		case val, ok := <-outCh:
+			require.False(t, ok, "output channel didn't close as expected")
+			assert.Zero(t, val)
+		default:
+			t.Fatal("debouncer didn't close the channel after the initial debounce duration")
+		}
+
+	})
+
+	t.Run("Simple debounce after initial duration, unlimited items specified", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		// setup an input channel where we inject a series of values very quickly
+		inCh := make(chan int)
+
+		debounceDuration := 50 * time.Millisecond
+
+		values := []int{0, 1, 42, 66, 99, 100, 999}
+		// send all values
+		go func() {
+			for _, v := range values {
+				inCh <- v
+			}
+		}()
+
+		maxItems := 0
+		outCh := AccumulatorDebounce(ctx, inCh, debounceDuration, maxItems)
+
+		select {
+		case val, ok := <-outCh:
+			require.Truef(t, ok, "output channel closed unexpectedly")
+			actualValues := []int{}
+			for _, v := range val {
+				actualValues = append(actualValues, v.Value)
+			}
+			assert.Equal(t, values, actualValues)
+		case <-ctx.Done():
+			t.Fatal("didn't receive a value before timeout")
+		}
+
+		select {
+		case val, ok := <-outCh:
+			require.False(t, ok, "output channel didn't close as expected")
+			assert.Zero(t, val)
+		default:
+			t.Fatal("debouncer didn't close the channel after the initial debounce duration")
+		}
+
+	})
+
+	t.Run("Simple debounce when value comes after minDuration", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		// setup an input channel where we inject a value
+		inCh := make(chan int)
+
+		debounceDuration := 10 * time.Millisecond
+		values := []int{0, 1, 2}
+		// send values after a delay greater than the minDebounce
+		go func() {
+			<-time.After(100 * time.Millisecond)
+			for _, v := range values {
+				inCh <- v
+			}
+
+		}()
+
+		maxItems := 5
+		outCh := AccumulatorDebounce(ctx, inCh, debounceDuration, maxItems)
+
+		select {
+		case val, ok := <-outCh:
+			require.Truef(t, ok, "output channel closed unexpectedly")
+			assert.Len(t, val, 1)
+			assert.Equal(t, values[0], val[0].Value)
+		case <-ctx.Done():
+			t.Fatal("didn't receive a value before timeout")
+		}
+
+		select {
+		case val, ok := <-outCh:
+			require.False(t, ok, "output channel didn't close as expected")
+			assert.Zero(t, val)
+		default:
+			t.Fatal("debouncer didn't close the channel after the debounce")
+		}
+
+	})
+
+	t.Run("Send value when input channel closed after values", func(t *testing.T) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		// setup an input channel where we inject values before closing
+		inCh := make(chan int)
+
+		// debounce duration is still within the context timeout but value should return after the channel closure and after the debounce
+		debounceDuration := 50 * time.Millisecond
+		values := []int{0, 1, 42}
+
+		// send values then close the input channel
+
+		go func() {
+			for _, v := range values {
+				inCh <- v
+			}
+
+			close(inCh)
+		}()
+
+		maxItems := 5
+		outCh := AccumulatorDebounce(ctx, inCh, debounceDuration, maxItems)
+
+		select {
+		case val, ok := <-outCh:
+			require.Truef(t, ok, "channel closed before we receive the value")
+			actualValues := []int{}
+			for _, v := range val {
+				actualValues = append(actualValues, v.Value)
+			}
+			assert.Equal(t, values, actualValues)
+
+		case <-ctx.Done():
+			t.Fatal("didn't receive a value before timeout")
+		}
+
+		select {
+		case val, ok := <-outCh:
+			assert.Falsef(t, ok, "channel should be closed after the values")
 			assert.Zero(t, val)
 		default:
 			t.Fatal("debouncer didn't close the output channel in time")
